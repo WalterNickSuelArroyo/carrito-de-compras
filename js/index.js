b8f9e78dba1a2016e50e50182be994340ae6bb73
WalterNickSuelArroyo <nicodehero21@outlook.com>
2024-08-21 15:41:38 -0500

    estructura y estilos del proyecto

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const contenedorTarjetas = document.getElementById("productos-container");
+function crearTarjetaProductosInicio(productos) {
+    productos.forEach(producto => {
+        const nuevaRopa = document.createElement("div");
+        nuevaRopa.classList = "tarjeta-producto"
+        nuevaRopa.innerHTML = `
+            <img src = "../img/productos/ropa${producto.id}.jpg" >
+            <h3>${producto.nombre}</h3>
+            <p>${producto.precio}</p>
+            <button>Agregar al carrito</button>
+        `
+        contenedorTarjetas.appendChild(nuevaRopa);
+    });
+}
+
+crearTarjetaProductosInicio(ropas)
